f084e93b1b343ae5eb3770c80db2985d8ccb6869
Tanya Bajpai <tanyabajpai2312@gmail.com>
2025-09-18 14:38:14 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name = "Generative AI",
+    version =' 0.0.0',
+    author_name="Tanya Bajpai",
+    author_email ='tanyabajpai2312@gmail.com',
+    packages = find_packages(),
+    install_requires = [] 
+)
